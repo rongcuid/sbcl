3bfd8655b4c01dfdae8f97269323cbb2cb255035
Rongcui Dong <rongcuid@outlook.com>
2024-05-14 12:50:36 -0400

    Slight refactor

--- tests/alien-struct-by-value.c
+++ tests/alien-struct-by-value.c
@@ -1,31 +1,35 @@
+/** A large structure with 8-byte alignment.
+    This should be too big for any architecture to pass by registers.
+*/
 struct large_align_8 {
-    long long m0, m4, m8, m12;
-    long long m1, m5, m9, m13;
-    long long m2, m6, m10, m14;
-    long long m3, m7, m11, m15;
+  long long m0, m4, m8, m12;
+  long long m1, m5, m9, m13;
+  long long m2, m6, m10, m14;
+  long long m3, m7, m11, m15;
 };
-#define large_align_8_test(member) \
-  long long large_align_8_test_##member(struct large_align_8 m) { \
-    return m.member; \
+#define large_align_8_get(member)                                \
+  long long large_align_8_get_##member(struct large_align_8 m) { \
+    return m.member;                                              \
   }
-large_align_8_test(m0);
-large_align_8_test(m1);
-large_align_8_test(m2);
-large_align_8_test(m3);
-large_align_8_test(m4);
-large_align_8_test(m5);
-large_align_8_test(m6);
-large_align_8_test(m7);
-large_align_8_test(m8);
-large_align_8_test(m9);
-large_align_8_test(m10);
-large_align_8_test(m11);
-large_align_8_test(m12);
-large_align_8_test(m13);
-large_align_8_test(m14);
-large_align_8_test(m15);
+large_align_8_get(m0);
+large_align_8_get(m1);
+large_align_8_get(m2);
+large_align_8_get(m3);
+large_align_8_get(m4);
+large_align_8_get(m5);
+large_align_8_get(m6);
+large_align_8_get(m7);
+large_align_8_get(m8);
+large_align_8_get(m9);
+large_align_8_get(m10);
+large_align_8_get(m11);
+large_align_8_get(m12);
+large_align_8_get(m13);
+large_align_8_get(m14);
+large_align_8_get(m15);
 
-void large_align_8_test_mutate(struct large_align_8 m) {
+/** Mutates the input struct. Volatile to avoid compiler optimizing away the mutation.*/
+void large_align_8_mutate(volatile struct large_align_8 m) {
   m.m0++;
   m.m1++;
   m.m2++;
--- tests/alien.impure.lisp
+++ tests/alien.impure.lisp
@@ -591,14 +591,15 @@
             (m1 (integer 64)) (m5 (integer 64)) (m9 (integer 64)) (m13 (integer 64))
             (m2 (integer 64)) (m6 (integer 64)) (m10 (integer 64)) (m14 (integer 64))
             (m3 (integer 64)) (m7 (integer 64)) (m11 (integer 64)) (m15 (integer 64))))
-(defmacro def-large-align-8-test (i)
-  (let ((lisp-name (sb-int:symbolicate "LARGE-ALIGN-8-TEST-M" i)))
+(defmacro def-large-align-8-get (i)
+  (let ((lisp-name (sb-int:symbolicate "LARGE-ALIGN-8-GET-M" i)))
     `(define-alien-routine ,lisp-name (integer 64) (m (struct large-align-8)))))
-(defmacro defs-large-align-8-test ()
-  (let ((defs (loop for i upto 15 collect `(def-large-align-8-test ,i))))
+(defmacro defs-large-align-8-get ()
+  "Test functions for each member"
+  (let ((defs (loop for i upto 15 collect `(def-large-align-8-get ,i))))
     `(progn ,@defs)))
-(defs-large-align-8-test)
-(define-alien-routine large-align-8-test-mutate void (m (struct large-align-8)))
+(defs-large-align-8-get)
+(define-alien-routine large-align-8-mutate void (m (struct large-align-8)))
 (with-test (:name :struct-by-value-large-align-8-args)
   (with-alien ((m (struct large-align-8)))
     (macrolet ((set-members ()
@@ -611,7 +612,7 @@
                (test-members ()
                  "Test that each member has correct value"
                  (loop for i upto 15
-                       collect (let ((f (sb-int:symbolicate "LARGE-ALIGN-8-TEST-M" i)))
+                       collect (let ((f (sb-int:symbolicate "LARGE-ALIGN-8-GET-M" i)))
                                  `(assert (= ,i (,f m))))
                          into tests
                        finally (return `(progn ,@tests)))))
@@ -620,7 +621,7 @@
       ;; Test that struct is correctly passed
       (test-members)
       ;; Call a C function that mutates struct locally
-      (large-align-8-test-mutate m)
+      (large-align-8-mutate m)
       ;; Test that the original struct is not modified
       (test-members))))
 
